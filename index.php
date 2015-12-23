e4b7e5a4477813881d320f1c3725ad5871556570
Dana Gregory <dana2048@hotmail.com>
2015-12-23 16:19:06 -0500

    Add Real Charts
    Add Temperature and Pressure charts with real data

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 require 'includes/db.php';	//database access
 require 'includes/template.php';	//template handler
 
+//echo '$_SERVER[TMP]=' . $_SERVER['TMP'] . '<br>';
+//echo 'sys_get_temp_dir =' . sys_get_temp_dir() . '<br>';;
+
 //########## get current temperature and humidity ##########
 $sql = 'SELECT * FROM data WHERE sensor=1 LIMIT 1';
 $sql = 
@@ -44,6 +47,8 @@ $yesterday = $value['yesterday'];
 $today = date("Y-m-d");
 $timeVal  = mktime(0, 0, 0, date("m")  , date("d")-1, date("Y"));
 $yesterday = date("Y-m-d", $timeVal);
+$thisMonth = date("m", $timeVal);
+$thisYear = date("Y", $timeVal);
 
 $sql = 
 "SELECT AVG(temperature) AS yesterdayTemp, AVG(humidity) AS yesterdayHumid " .
@@ -60,10 +65,77 @@ $values['yesterdayHumid'] = $value['yesterdayHumid'];
 //$a = array(11,3,8,12,5,1,9,13,5,7);
 //$values['chart'] = $a;
 
-//CHART
+//###################################################
+//TEMPERATURE CHART SENSOR 1
 $sql = 
 "SELECT temperature " .
 "FROM data WHERE sensor=1 AND DATE(stamp)='" . $yesterday . "'";
+
+$result = $db->query($sql);
+
+$t1 = array();
+	//echo '<pre>';
+	while ( $row = $result->fetch_array(MYSQL_ASSOC) ) 
+	{
+		$t1[] = $row['temperature'];
+			//print_r($row); echo "<br>";
+	}
+
+	//echo '</pre><hr>';
+$values['chartTemperature1'] = $t1;
+
+
+//###################################################
+//TEMPERATURE CHART SENSOR 2
+$sql = 
+"SELECT temperature " .
+"FROM data WHERE sensor=2 AND DATE(stamp)='" . $yesterday . "'";
+
+$result = $db->query($sql);
+
+$t2 = array();
+	//echo '<pre>';
+	while ( $row = $result->fetch_array(MYSQL_ASSOC) ) 
+	{
+		$t2[] = $row['temperature'];
+			//print_r($row); echo "<br>";
+	}
+
+	//echo '</pre><hr>';
+$values['chartTemperature2'] = $t2;
+
+
+//PRESSURE CHART
+$sql = 
+"SELECT pressure " .
+"FROM data WHERE sensor=3 AND YEAR(stamp)='" . $thisYear . "' AND MONTH(stamp)='" . $thisMonth . "'";
+
+$result = $db->query($sql);
+
+$t = array();
+	//echo '<pre>';
+	while ( $row = $result->fetch_array(MYSQL_ASSOC) ) 
+	{
+		$t[] = $row['pressure'];
+			//print_r($row); echo "<br>";
+	}
+
+	//echo '</pre><hr>';
+$values['chartPressure'] = $t;
+
+//new instance of template handler for INDEX template
+//$tpl = new Template('index');
+
+//pass these variables to our template
+//$tpl->vars = array(
+//	'values' => $values
+//);
+
+
+//MONTH TEMPERATURE CHART
+$sql = 
+"SELECT temperature " .
+"FROM data WHERE sensor=1 AND YEAR(stamp)='" . $thisYear . "' AND MONTH(stamp)='" . $thisMonth . "'";
 
 $result = $db->query($sql);
 
@@ -76,8 +148,10 @@ $t = array();
 	}
 
 	//echo '</pre><hr>';
-$values['chart'] = $t;
+$values['chartMonthTemperature'] = $t;
 
+
+//PASS VALUES TO TEMPLATE FOR DISPLAY
 //new instance of template handler for INDEX template
 $tpl = new Template('index');
 
--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -4,6 +4,15 @@
 <?php
 require_once ('jpgraph/jpgraph.php');
 require_once ('jpgraph/jpgraph_line.php');
+
+//echo 'USE_CACHE =';
+//print_r(USE_CACHE);
+//echo '<br>';
+
+//echo 'CACHE_DIR =';
+//print_r(CACHE_DIR);
+//echo '<br>';
+
 ?>
 
 <!-- CURRENT CONDITIONS -->
@@ -47,17 +56,56 @@ require_once ('jpgraph/jpgraph_line.php');
 </table>
 
 
-<!-- CHART -->
-<h2>CHART</h2>
-
+<!-- TEMPERATURE CHART -->
+<h2>Yesterday's Temperature Chart</h2>
 
 <?php
 // Some data
 //$ydata = array(11,3,8,12,5,1,9,13,5,7);
-$ydata = $values['chart'];
+//$chartFileName = sys_get_temp_dir() . '\\' . 'chartTemperature.png';
+$chartFileName = 'chartTemperature.png';
 
 // Create the graph. These two calls are always required
-$graph = new Graph(1024,480,$aInline=false);
+$graph = new Graph(1024,480,$chartFileName,100,$aInline=false);
+$graph->SetScale('textlin');
+
+// SENSOR 1 - 
+//Create the linear plot
+$lineplot=new LinePlot($values['chartTemperature1']);
+$lineplot->SetColor('darkgreen');
+$lineplot->SetStyle('dashed');
+
+// Add the plot to the graph
+$graph->Add($lineplot);
+
+// SENSOR 2 - 
+//Create the linear plot
+$lineplot=new LinePlot($values['chartTemperature2']);
+$lineplot->SetColor('darkred');
+$lineplot->SetStyle('dotted');
+
+// Add the plot to the graph
+$graph->Add($lineplot);
+
+// Display the graph
+$graph->Stroke($chartFileName);
+//echo '<img src="' . $chartFileName . '">'
+//echo '<img src=file:///' . $chartFileName . '>';
+echo '<img src="' . $chartFileName . '">';
+?>
+
+
+<!-- PRESSURE CHART -->
+<h2>This Month's Pressure Chart</h2>
+
+<?php
+$ydata = $values['chartPressure'];
+//print_r($ydata);
+$chartFileName = sys_get_temp_dir() . '\\' . 'chartPressure.png';
+$chartFileName = 'chartPressure.png';
+
+// Create the graph. These two calls are always required
+$graph = new Graph(1024,480,$chartFileName,100,$aInline=false);
 $graph->SetScale('textlin');
 
 // Create the linear plot
@@ -68,7 +116,31 @@ $lineplot->SetColor('green');
 $graph->Add($lineplot);
 
 // Display the graph
-$graph->Stroke('auto');
+$graph->Stroke($chartFileName);
+echo '<img src="' . $chartFileName . '">';
 ?>
 
-<img src="Index.png">
+<!-- MONTH TEMPERATURE CHART -->
+<h2>This Month's Temperature Chart</h2>
+
+<?php
+$ydata = $values['chartMonthTemperature'];
+//print_r($ydata);
+$chartFileName = sys_get_temp_dir() . '\\' . 'chartMonthTemperature.png';
+$chartFileName = 'chartMonthTemperature.png';
+
+// Create the graph. These two calls are always required
+$graph = new Graph(1024,480,$chartFileName,100,$aInline=false);
+$graph->SetScale('textlin');
+
+// Create the linear plot
+$lineplot=new LinePlot($ydata);
+$lineplot->SetColor('green');
+
+// Add the plot to the graph
+$graph->Add($lineplot);
+
+// Display the graph
+$graph->Stroke($chartFileName);
+echo '<img src="' . $chartFileName . '">';
+?>
